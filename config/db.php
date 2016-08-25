fd89ff0c6f057e6ef0c58b84e8d11e66799c5582
carlositos <a.tykhonchuk@nozo.com>
2016-08-25 10:22:03 +0300

    db config to default

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=developex',
-    'username' => 'root',
-    'password' => 'q1w2e3Q@',
+    'username' => 'username',
+    'password' => 'password',
     'charset' => 'utf8',
 ];
